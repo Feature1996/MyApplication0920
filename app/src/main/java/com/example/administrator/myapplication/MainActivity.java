9f5768c93baeb78aa15b9e8fae68e9ad55487b3a
Feature1996 <1147283187@qq.com>
2018-09-20 20:07:15 +0800

    2018.9.20第二节更新 学习了button的监听以及获取textView中数据

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        TextView textView = (TextView) findViewById(R.id.text1);
+        textView.setText("Hello zhang");
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("this is a listner","hello");
+                EditText editText = (EditText) findViewById(R.id.editText);
+                TextView textView = (TextView) findViewById(R.id.text1);
+                String str= editText.getText().toString();
+                textView.setText("Hello listner I am "+str);
+            }
+        });
     }
 }
